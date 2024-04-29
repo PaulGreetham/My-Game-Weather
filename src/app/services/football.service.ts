9aa4ea787115a0c70628b97b509b7c5532f8c9de
Paul Greetham <pgreetham@protonmail.com>
2024-04-29 13:08:10 +0200

    env route update

--- src/app/services/football.service.ts
+++ src/app/services/football.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Team } from '../interfaces/team.interface';
-import { environment } from '../../environments/environment';
+import { environment } from '../../environments/environment.prod'
 
 @Injectable({
   providedIn: 'root',
